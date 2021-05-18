75c92dcbd1ae76edd91f90e9eef490d36a3cf92c
Papidev <lzanzottera@gmail.com>
2021-05-19 01:22:46 +0200

    refactoring index page

--- lib/components/open-graph.tsx
+++ lib/components/open-graph.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
-import meta from '~/data/meta.json'
+import meta from "@/data/meta.json";
 
-export const OpenGraph: React.FC<{title: string, description: string, image?: string}> = ({title, description, image}) => {
-  return <Head>
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:site_name" content={meta.name} />
-    <meta name="description" content={description} />
-    <meta property="og:image" content={image ? image : `${meta.productionUrl}/img/social/social.jpg`} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:creator" content={meta.author.social.twitterHandle} />
-  </Head>
-}
+export const OpenGraph: React.FC<{
+  title: string;
+  description: string;
+  image?: string;
+}> = ({ title, description, image }) => {
+  return (
+    <Head>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={meta.name} />
+      <meta name="description" content={description} />
+      <meta
+        property="og:image"
+        content={image ? image : `${meta.productionUrl}/img/social/social.jpg`}
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={meta.author.social.twitterHandle} />
+    </Head>
+  );
+};
