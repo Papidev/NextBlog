a18c965a3a25cf2bbf6a7c4ec7f8c27af7a4f3d0
Luca Zanzottera <luca@mirta.com>
2021-07-17 18:26:17 +0200

    kerguelend mdx: added image component

--- lib/components/Image/Image.tsx
+++ lib/components/Image/Image.tsx
@@ -1,46 +1,51 @@
-import React from "react";
+import React from 'react';
 
 // import styles from "./CaptionImage.module.css";
 
 // import { viewportContext } from "components/ViewportProvider";
 // import styles from "./CaptionImage.module.css";
 
-const imagesLib = require("lib/images");
-const constants = require("lib/constants");
+const imagesLib = require('lib/images');
+const constants = require('lib/constants');
 
-export default function CaptionImage({
-  section,
-  imgName,
-  description,
-  alternativeImgName,
-  // children,
-}) {
-  console.log("section", section);
-  const srcSet = imagesLib.getImgSrcset(section, imgName, constants.imgSizes);
-  const alternativesrcSet = imagesLib.getImgSrcset(
-    section,
-    alternativeImgName,
-    constants.imgSizes
-  );
+interface IImage {
+	section: string;
+	imgName: string;
+	description?: string;
+	alternativeImgName?: string;
+}
 
-  // const { isNarrowScreen } = React.useContext(viewportContext);
-  // let { width, height } = imagesLib.getImgInfo(path);
-  return (
-    <>
-      <picture>
-        <source srcSet={alternativesrcSet} media="(max-width: 600px)" />
-        <img
-          src={`${imgName}.jpg`}
-          alt={`${description}`}
-          srcSet={srcSet}
-          sizes="100vw"
-          // className={styles["content-img"]}
-        ></img>
-      </picture>
+export const Image = ({
+	section,
+	imgName,
+	description,
+	alternativeImgName,
+}: IImage) => {
+	const srcSet = imagesLib.getImgSrcset(section, imgName, constants.imgSizes);
+	const alternativesrcSet = imagesLib.getImgSrcset(
+		section,
+		alternativeImgName,
+		constants.imgSizes
+	);
 
-      {/* <p className={styles["img-citation"]}>
+	// const { isNarrowScreen } = React.useContext(viewportContext);
+	// let { width, height } = imagesLib.getImgInfo(path);
+	return (
+		<>
+			<picture>
+				<source srcSet={alternativesrcSet} media='(max-width: 600px)' />
+				<img
+					src={`${imgName}.jpg`}
+					alt={`${description}`}
+					srcSet={srcSet}
+					sizes='100vw'
+					// className={styles["content-img"]}
+				></img>
+			</picture>
+
+			{/* <p className={styles["img-citation"]}>
         <cite dangerouslySetInnerHTML={createMarkup(children)} />
       </p> */}
-    </>
-  );
-}
+		</>
+	);
+};
